c31098510740ebe9325f8aafac1e52718950fa26
Yahaira Adama <yahairaadamagalarza@gmail.com>
2020-08-03 10:19:06 -0500

    Arquitectura MVC - Con Archivos Listos (renombrado)

--- src/java/Modelo/Libro.java
+++ src/java/Modelo/Libro.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ModeloDAO;
+package Modelo;
 
 /**
  *
  * @author Usuario
  */
-public class BibliotecaDAO {
+public class Libro {
     
 }
--- src/java/ModeloDAO/LibroDAO.java
+++ src/java/ModeloDAO/LibroDAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Modelo;
+package ModeloDAO;
 
 /**
  *
  * @author Usuario
  */
-public class Biblioteca {
+public class LibroDAO {
     
 }
